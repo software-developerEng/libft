e8e3e3678aeecd87e9a7eaa37bcd04331b41e8ce
najm-choueiry <nachoueiry@outlook.com>
2024-06-10 20:03:18 +0300

    as

---
+++ functions.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchoueir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 14:21:05 by nchoueir          #+#    #+#             */
+/*   Updated: 2024/06/10 14:21:07 by nchoueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RUSH_H
+#define RUSH_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int ft_isalpha(char *s1);
+int ft_isdigit(char *s1);
+int ft_isalnum(char *s1);
+
+#endif
