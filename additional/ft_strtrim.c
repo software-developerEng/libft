741989ce8bc6f3f1cf4a60cbacea5d8b31ec685a
software-developerEng <145709128+software-developerEng@users.noreply.github.com>
2024-06-15 07:24:22 +0300

    ft_strtrim (not done)

---
+++ additional/ft_strtrim.c
@@ -0,0 +1,24 @@
+int str_len(char *s1)
+{
+    int i = 0;
+    while (s1[i])  
+        i++;
+    return i;
+}
+
+
+char storingTheArray(int number)
+{
+    char *str;
+
+    str = (char *)malloc(sizeof * (str_len(s1) + str_len(s2) ))
+    if (!str)
+        return NULL;
+    return str;
+}
+
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+
+}
