88f67285f19f794e603b2f62a615cca4a557ad57
software-developerEng <145709128+software-developerEng@users.noreply.github.com>
2024-06-15 07:04:52 +0300

    ft_strjoin

--- additional/ft_strjoin.c
+++ additional/ft_strjoin.c
@@ -1,6 +1,35 @@
+
+static size_t str_len(char const *str)
+{
+    size_t i;
+    i = 0;
+    while (*(str + i))
+        i++;
+    return (i);
+}
+
+static char *str_new(size_t n)
+{
+        char *str;
+
+    str = (char *)malloc(sizeof(char) * (n + 1));
+    if(!str)
+        return  (NULL);
+    return str;
+}
+
+
 char *ft_strjoin(char const *s1, char const *s2)
 {
-//     s1: The prefix string.
-//      s2: The suffix string.
+    char *str;
+    char *str_ptr;
+
+    if (!s1 || !s2)
+        return Null;
+    str = str_new(str_len(s1) + str_len(s2));
+    str_ptr = str;
+    while (*s1)
+        *str++ s1++
+
     
 }
